1255660cc94a74765a14cf9ffe879eccfd9df721
taekrigi <taekrigi@gmail.com>
2021-02-24 22:59:04 +0900

    mapstruct 테스트를 위해 lombok 추가안함

---
+++ src/main/java/travelmap/TravelmapApplication.java
@@ -0,0 +1,13 @@
+package travelmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/travelmap/TravelmapApplicationTests.java
@@ -0,0 +1,13 @@
+package travelmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
