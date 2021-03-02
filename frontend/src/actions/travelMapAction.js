a26299ba8e57e677ea5ac85e054b5c7325c72de0
taekrigi <taekrigi@gmail.com>
2021-03-03 01:09:57 +0900

    React hardcoding travelmap before mysql being connected

--- frontend/src/actions/travelMapAction.js
+++ frontend/src/actions/travelMapAction.js
@@ -10,13 +10,58 @@ export const getTravelMaps = userId => async (disaptch) => {
   try {
     disaptch({ type: TRAVEL_MAP_LIST_REQUEST })
 
-    const { data } = await axios.get(
-      `http://localhost:8080/travelmaps/${userId}`
-    )
+    // TODO: SET REAL DATA
+    // const { data } = await axios.get(
+    //   `http://localhost:8080/travelmaps/${userId}`
+    // )
 
     disaptch({
       type: TRAVEL_MAP_LIST_SUCCESS,
-      payload: data,
+      payload: { 
+        travelMaps: 
+          [
+            { 
+              country: 'kr', 
+              value: 1
+            },
+            {
+              country: 'jp',
+              value: 2
+            },
+            {
+              country: 'nz',
+              value: 3
+            },
+            {
+              country: 'de',
+              value: 4
+            },
+            {
+              country: 'at',
+              value: 5
+            },
+            {
+              country: 'cz',
+              value: 6
+            },
+            {
+              country: 'fr',
+              value: 7
+            },
+            {
+              country: 'tw',
+              value: 8
+            },
+            {
+              country: 'vn',
+              value: 9
+            },
+            {
+              country: 'ch',
+              value: 10
+            },
+          ] 
+      },
     })
   } catch (error) {
     disaptch({
