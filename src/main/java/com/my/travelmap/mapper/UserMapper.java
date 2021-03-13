a760235edf7abfb4fb3a69fea1863bf9e6c0dafc
taekrigi <taekrigi@gmail.com>
2021-03-13 23:15:50 +0900

    setting mapstruct ignore policy but i don't know why spring cannnot find the mapstruct impl everytime i changed mapper source code...

--- src/main/java/com/my/travelmap/mapper/UserMapper.java
+++ src/main/java/com/my/travelmap/mapper/UserMapper.java
@@ -2,18 +2,16 @@ package com.my.travelmap.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
 import com.my.travelmap.dto.UserDto;
 import com.my.travelmap.entity.User;
 import com.my.travelmap.mapper.base.BaseMapper;
 import com.my.travelmap.param.UserParam;
 
-@Mapper(componentModel = "spring", uses = PasswordEncoderMapper.class)
+@Mapper(componentModel = "spring", uses = PasswordEncoderMapper.class, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper extends BaseMapper<User, UserDto, UserParam> {
 
-	@Mapping(target = "authorities", ignore = true)
-	@Mapping(target = "travelMaps", ignore = true)
-	@Mapping(target = "userDetail", ignore = true)
 	@Mapping(source = "password", target = "encryptedPassword", qualifiedBy = EncodedMapping.class)
 	User toEntity(UserParam param);
 }
